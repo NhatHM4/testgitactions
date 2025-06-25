f48b2e70e8602ead90d3f21e7cb90fce022a65a1
NhatHM4 <haminhnhat711@gmail.com>
2025-06-25 22:40:00 +0700

    test lable

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("Hello, world!");
 console.log("This is a simple Node.js script that prints 'Hello, world!' to the console.");
-console.log("You can run this script using Node.js by executing 'node index.js' in your terminal.");
+console.log("You can run this script using Node.js by executing 'node index.js' in your terminal1.");
 // This is a simple Node.js script that prints "Hello, world!" to the console.
