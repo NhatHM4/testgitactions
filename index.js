25567b686c9ca0d4dacfae29ce5acc5d53d50cb9
NhatHM4 <haminhnhat711@gmail.com>
2025-06-25 22:48:19 +0700

    z

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("Hello, world!");
 console.log("This is a simple Node.js script that prints 'Hello, world!' to the console.");
-console.log("You can run this script using Node.js by executing 'node index.js' in yo1ur terminalaccccs.");
+console.log("You can run this script using Node.js by executing 'node index.js' in yo1ur termina2laccccs.");
 // This is a simple Node.js script that prints "Hello, world!" to the console.
